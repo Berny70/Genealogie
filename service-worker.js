15fd08c17eb30c5c86eb8fb505e61dfd3895cd18
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-02-02 17:13:05 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "genealogie-v1";
+const ASSETS = [
+  "/Genealogie/",
+  "/Genealogie/index.html",
+  "/Genealogie/app.js",
+  "/Genealogie/genealogie.json",
+  "/Genealogie/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
